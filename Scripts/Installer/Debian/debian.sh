9bc7f0399d9237c280ef1ecebd1652453a9a3f7b
kamput520 <70871119+kamput520@users.noreply.github.com>
2021-01-28 15:19:24 +0800

    Update debian.sh

--- Scripts/Installer/Debian/debian.sh
+++ Scripts/Installer/Debian/debian.sh
@@ -24,7 +24,7 @@ if [ "$first" != 1 ];then
 		*)
 			echo "unknown architecture"; exit 1 ;;
 		esac
-		wget "https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Rootfs/Debian/${archurl}/debian-rootfs-${archurl}.tar.xz" -O $tarball
+		wget "https://raw.github.com/kamput520/AnLinux-Resources/blod/master/Rootfs/Debian/${archurl}/debian-rootfs-${archurl}.tar.xz" -O $tarball
 	fi
 	cur=`pwd`
 	mkdir -p "$folder"
